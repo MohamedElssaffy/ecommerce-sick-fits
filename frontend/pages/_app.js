6cdcf96d81058dffc409b392eb30efac78e78ce8
Mohamed Elssaffy <ma7amad123410000@gmail.com>
2021-09-27 02:29:14 +0200

    connect next with apollo cliet

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,22 +2,40 @@
 import PropTypes from 'prop-types';
 import Router from 'next/router';
 import NProgress from 'nprogress';
+import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
 import '../components/styles/nprogress.css';
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+
+  return pageProps;
+};
 
 MyApp.propTypes = {
   Component: PropTypes.elementType,
   pageProps: PropTypes.object,
+  apollo: PropTypes.object,
 };
+
+export default withData(MyApp);
